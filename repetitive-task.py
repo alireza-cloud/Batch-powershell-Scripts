e0bb3f05901135a027ccc2bac2b77c8acfc57a92
alireza-cloud <62068604+alireza-cloud@users.noreply.github.com>
2023-03-09 14:49:07 +0100

    Update repetitive-task.py

--- repetitive-task.py
+++ repetitive-task.py
@@ -2,5 +2,4 @@
 n = 1200
 for x in range (n):
     Column_1, Column_2, Column_3 = input().split()
-    #print(f"Update Endpoints set Path = '{lat}' Address='{long}' Name='{long}' Channel='{long}' BaudRate='{long}' WHERE BasestationId = '{x+1}'")
     print(f"INSERT INTO Table_name (Column_1, Column_2, Column_3) VALUES (1, '{Column_1}', {Column_2}, '{Column_3}');")
